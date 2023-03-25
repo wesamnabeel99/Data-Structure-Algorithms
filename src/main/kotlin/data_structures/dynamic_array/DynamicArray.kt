cd473c1265d12fa6bb59468208f96befb7174e96
Wesam Nabeel <wesamezio@gmail.com>
2023-03-26 01:47:04 +0300

    implement dynamic array (not completed)

--- src/main/kotlin/data_structures/dynamic_array/DynamicArray.kt
+++ src/main/kotlin/data_structures/dynamic_array/DynamicArray.kt
@@ -1,5 +1,64 @@
 package data_structures.dynamic_array
 
-class DynamicArray {
-    // TODO : implement dynamic array
+class DynamicArray<T>(capacity: Int) : Iterable<T?> {
+
+    private lateinit var array: Array<T?>
+
+    private var length = 0
+
+    private var capacity = 0
+
+    init {
+        if (capacity < 0) {
+            throw IllegalArgumentException("Illegal capacity $capactiy")
+        }
+
+        this.capacity = capacity
+
+        array = arrayOfNulls<T>(capacity)
+    }
+    fun isEmpty() = (length == 0)
+    fun get(index:Int): T? = array[index]
+    fun set(index:Int, element: T) {
+        array[index] = element
+    }
+    fun clear() {
+        for (i in 0.until(capacity)) {
+            array[i] = null
+        }
+        length = 0
+    }
+
+    fun add(element: T) {
+        if (length + 1 > capacity) {
+            doubleTheArray()
+        }
+        array[length++] = element
+    }
+
+    private fun doubleTheArray() {
+        if (capacity == 0) {
+            capacity = 1
+        } else {
+            capacity *=2
+        }
+
+        val newArray = arrayOfNulls<T>(capacity)
+
+        for (i in 0.until(capacity)) {
+            newArray[i] = array[i]
+        }
+
+        array = newArray
+    }
+
+    override fun iterator(): Iterator<T?> {
+        return object : Iterator<T?> {
+            var index =0
+            override fun hasNext() = index < length
+
+            override fun next() :T? =  array[index++]
+        }
+    }
+
 }
